edf4bb5c51af6185d82f130c013e48fb69892bc4
Anton Zakharov <c0rnota@gmail.com>
2017-11-26 00:16:58 +0300

    first database request

---
+++ app/Http/Controllers/Gorelka.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Gorelka extends Controller
+{
+    public function GetData()
+    {
+        $alldata = DB::table('gorelkadata')->orderBy('datetime','desc')->limit(1440)->get();
+        return $alldata;
+    }
+}
